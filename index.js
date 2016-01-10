af05238bdc766d9b7ca512ccea4c5f1993adc81c
Cody B. Daig <cody.daig@hackreactor.com>
2016-01-10 13:01:01 -0800

    First attempt at storing messages to cassandra:

--- index.js
+++ index.js
@@ -2,39 +2,44 @@
 
 var Slack = require('slack-client');
 
+var db = require('./lib/db.js');
 var commands = require('./lib/commands.js');
 
 var token = '' || process.env.SLACK_API_TOKEN;
 
 var slack = new Slack(token, true, true);
 
-slack.on('open', function () {
-  console.log("Connected to ", slack.team.name, "  as @", slack.self.name);
-});
+db.start(function () {
+  slack.on('open', function () {
+    console.log("Connected to ", slack.team.name, "  as @", slack.self.name);
+  });
 
-slack.on('message', function (message) {
-  console.log('A new message: ', message.text);
+  slack.on('message', function (message) {
+    console.log('A new message: ', message.text);
 
-  var channel = slack.getChannelGroupOrDMByID(message.channel);
-  
-  // Check to see if @codybot was tagged
-  if(message.text.indexOf('<@U0J3GQGQ4>') === 0) {
-    // codybot was tagged. 
+    db.insertMessage(message.type, message.channel, message.user, message.text, message.ts, message.team);
 
-    var msgArr = message.text.split(' ');
-    var cmd = msgArr[1];
-    console.log("cmd: ", cmd);
-    if(typeof commands[cmd] === 'function') {
-      msgArr.splice(0,2);
-      commands[cmd](msgArr, message);
-    } else {
-      channel.send('codybot doesn\'t recognize the command you specified. Teach him by submitting a PR!');
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
+    
+    // Check to see if @codybot was tagged
+    if(message.text.indexOf('<@U0J3GQGQ4>') === 0) {
+      // codybot was tagged. 
+
+      var msgArr = message.text.split(' ');
+      var cmd = msgArr[1];
+      console.log("cmd: ", cmd);
+      if(typeof commands[cmd] === 'function') {
+        msgArr.splice(0,2);
+        commands[cmd](msgArr, message);
+      } else {
+        channel.send('codybot doesn\'t recognize the command you specified. Teach him by submitting a PR!');
+      }
     }
-  }
-});
+  });
 
-slack.on('error', function (err) {
-  console.error(err);
-});
+  slack.on('error', function (err) {
+    console.error(err);
+  });
 
-slack.login();
+  slack.login();
+});
