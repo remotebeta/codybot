8fd4ed20cd1a6394e2717cc139cbfa33a20188f5
Cody B. Daig <cody.daig@hackreactor.com>
2016-01-10 12:04:17 -0800

    Basic Reject command

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var Slack = require('slack-client');
 
+var commands = require('./lib/commands.js');
+
 var token = '' || process.env.SLACK_API_TOKEN;
 
 var slack = new Slack(token, true, true);
@@ -12,10 +14,22 @@ slack.on('open', function () {
 
 slack.on('message', function (message) {
   console.log('A new message: ', message.text);
+
+  var channel = slack.getChannelGroupOrDMByID(message.channel);
   
-  // var channel = slack.getChannelGroupOrDMByID(message.channel);
-  
-  // channel.send('codybot has recieved your message but doesn\'t know what to do.');
+  // Check to see if @codybot was tagged
+  if(message.text.indexOf('<@U0J3GQGQ4>') === -1) {
+    // codybot was tagged. 
+
+    var msgArr = message.text.split(' ');
+    var cmd = msgArr[1];
+    if(typeof commands[cmd] === 'function') {
+      msgArr.splice(0,2);
+      commands[cmd](msgArr, message);
+    } else {
+      channel.send('codybot doesn\'t recognize the command you specified. Teach him by submitting a PR!');
+    }
+  }
 });
 
 slack.on('error', function (err) {
