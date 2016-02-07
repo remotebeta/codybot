2a9f92a5a1fbf495f786f78697729fd925e92b04
Cody B. Daig <cody.daig@hackreactor.com>
2016-02-06 20:12:11 -0700

    debugging

--- commands/telegraph.js
+++ commands/telegraph.js
@@ -6,7 +6,7 @@ function telegraph (slack, args, message) {
   var userID = args.shift();
   var msg = args.join(' ');
   console.log(userID);
-  userID = userID.substr(2, userID.length-1);
+  userID = userID.substr(2, userID.length-2);
   console.log(userID);
 
   var channel = slack.getChannelGroupOrDMByID(message.channel);
