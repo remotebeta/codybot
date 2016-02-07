c68fe1937c704fbd510bb54ece4d138cdbcb8dd6
Cody B. Daig <cody.daig@hackreactor.com>
2016-02-06 21:02:13 -0700

    Clean up telegraph

--- commands/telegraph.js
+++ commands/telegraph.js
@@ -5,14 +5,11 @@ module.exports = telegraph;
 function telegraph (slack, args, message) {
   var userID = args.shift();
   var msg = args.join(' ');
-  console.log(userID);
   userID = userID.slice(2, userID.length-1);
-  console.log(userID);
 
   var channel = slack.getChannelGroupOrDMByID(message.channel);
   channel.send('Sending a DM to: ' + userID);
 
-  console.log('dmChannel');
   slack.openDM(userID, function (openDmData) {
     if(openDmData.ok) {
       var dmChannel = slack.getChannelGroupOrDMByID(openDmData.channel.id);
