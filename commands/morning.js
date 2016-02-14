7e58789e76ea0cb7c7a2c9b9d56306e279b49823
Thr1ve <fict1on.buhler@gmail.com>
2016-02-14 17:01:27 -0500

    cleanup

--- commands/morning.js
+++ commands/morning.js
@@ -1,8 +1,6 @@
 'use strict';
 
-module.exports = morning;
-
-function morning(controller) {
+module.exports = function morning(controller) {
   controller.hears('morning', ['direct_mention'], function(bot, message) {
     bot.reply(message, 'Goooooooooood Morning!!!!!!!!!!');
   });
