5d45cf21f2121e2c0dd91c943d148ad1bb23b8a1
Thr1ve <fict1on.buhler@gmail.com>
2016-02-14 13:45:02 -0500

    cleanup

--- commands/beatbox.js
+++ commands/beatbox.js
@@ -2,11 +2,6 @@
 
 module.exports = beatbox;
 
-// function beatbox(slack, args, message) {
-//   var channel = slack.getChannelGroupOrDMByID(message.channel);
-//   channel.send('Boots and Cats and Boots and Cats and Boots and Cats and Boots and Cats and Cats and Boots and Cats and Boots and Cats and Boots and Cats and Boots.');
-// }
-
 function beatbox(controller) {
   controller.hears('beatbox', ['direct_mention'], function(bot, message) {
     console.log(message);
