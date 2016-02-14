8475951f1ee23c08e9d46663a69ec6bf6ba9ed12
Thr1ve <fict1on.buhler@gmail.com>
2016-02-14 13:43:57 -0500

    add working rejection command

--- commands/reject.js
+++ commands/reject.js
@@ -1,36 +1,35 @@
-module.exports = function caps() {};
-// 'use strict';
+'use strict';
 
-// var mongoose = require('mongoose');
+var Rejection = require('../lib/mongoose.js').Rejection;
 
-// var Rejection = mongoose.model('Rejection');
+module.exports = function reject(controller) {
+  controller.hears('reject', ['direct_mention'], function(bot, message) {
+    // This feels a bit hackish...is there a better way to do this?
+    var companyName = message.text.split(' ').slice(1).join(' ');
 
-// module.exports = reject;
+    var rejection = new Rejection({
+      companyName: companyName,
+      user: message.user,
+      timestamp: { type: Date, default: Date.now }
+    });
 
-// function reject(slack, args, message) {
-//   var channel = slack.getChannelGroupOrDMByID(message.channel);
-
-//   var companyName = args[0];
-//   var rejection = new Rejection({
-//     companyName: companyName,
-//     user: message.user,
-//     timestamp: { type: Date, default: Date.now }
-//   });
-
-//   Rejection.find({
-//     companyName: companyName,
-//     user: message.user
-//   }).exec(function (err, rejections) {
-//     if(rejections.length > 0) {
-//       channel.send('This information is already in my database. Thanks for reminding me though.');
-//     } else {
-//       rejection.save(function (err, rejection) {
-//         if (err) {
-//           return console.error(err);
-//         }
-
-//         channel.send('Sounds like you\'re too awesome for ' + rejection.companyName + ' anyways!');
-//       });
-//     }
-//   });
-// }
+    Rejection.find({
+      companyName: companyName,
+      // The message.user with botkit isn't actually the literal username
+      // Rather, it seems to be a hashed string based off the username
+      // For instance: for my username, 'fict1on', message.user is 'U0LFRMESZ'
+      user: message.user
+    }).exec(function (err, rejections) {
+      if(rejections.length > 0) {
+        bot.reply(message, 'This information is already in my database. Thanks for reminding me though.');
+      } else {
+        rejection.save(function (err, rejection) {
+          if (err) {
+            return console.error(err);
+          }
+          bot.reply(message, 'Sounds like you\'re too awesome for ' + rejection.companyName + ' anyways!');
+        });
+      }
+    });
+  })
+}
