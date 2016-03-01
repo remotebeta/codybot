d77f00085c9fe8ae084e5c7c3a05338ae37388d8
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-01 11:41:52 -0700

    debugging

--- commands/test.js
+++ commands/test.js
@@ -5,5 +5,5 @@ module.exports = test;
 function test(slack, args, message) {
   var channel = slack.getChannelGroupOrDMByID(message.channel);
   channel.send('All systems normal.');
-  console.log(message._client.channels.C0LFUN4F2);
+  console.log(message._client.channels.C0LFUN4F2.name);
 }
