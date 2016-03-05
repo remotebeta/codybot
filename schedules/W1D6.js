8663569724c488ceb82ee2733bbf08dc1efb71fd
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-05 10:16:08 -0700

    W1D6

--- schedules/W1D6.js
+++ schedules/W1D6.js
@@ -6,27 +6,27 @@ var date = require('../lib/date.js');
 var events = [];
 
 events.push({
-  when: date('W1D5', 12, 10),
+  when: date('W1D6', 12, 10),
   who: c.juniors,
   message: '<!channel> 5 minutes until Kitchen Time.'
 });
 
 events.push({
-  when: date('W1D5', 12, 14),
+  when: date('W1D6', 12, 14),
   who: c.juniors,
   message: '<!channel> 1 minute until Kitchen Time.'
 });
 
 events.push({
-  when: date('W1D5', 16, 40),
+  when: date('W1D6', 14, 25),
   who: c.juniors,
-  message: '<!channel> Junior/Senior Code Review in 5 minutes'
+  message: '<!channel> 5 minutes until your first Sprint Reflection.'
 });
 
 events.push({
-  when: date('W1D5', 16, 44),
+  when: date('W1D6', 14, 29),
   who: c.juniors,
-  message: '<!channel> Junior/Senior Code Review in 1 minute'
+  message: '<!channel> 1 minutes until your first Sprint Reflection.'
 });
 
 module.exports = events;
