90b5a92182f6941bacf4dda61e6f2cd97572e197
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-05 10:39:21 -0700

    Live Lecture Today

--- schedules/W1D6.js
+++ schedules/W1D6.js
@@ -29,4 +29,34 @@ events.push({
   message: '<!channel> 1 minutes until your first Sprint Reflection.'
 });
 
+events.push({
+  when: date('W1D6', 15, 30),
+  who: c.juniors,
+  message: '<!channel> Townhall in 15 minutes. Post your questions here: https://townhall.makerpass.com/'
+});
+
+events.push({
+  when: date('W1D6', 15, 40),
+  who: c.juniors,
+  message: '<!channel> 5 Minutes until TownHall. https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W1D6', 15, 44),
+  who: c.juniors,
+  message: '<!channel> 1 Minute until TownHall. https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W1D6', 16, 40),
+  who: c.juniors,
+  message: '<!channel> 5 Minutes until the Live Lecture on jQuery and Open Source Contributions. https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W1D6', 16, 44),
+  who: c.juniors,
+  message: '<!channel> 1 Minute until the Live Lecture on jQuery and Open Source Contributions. https://zoom.us/j/685772​608'
+});
+
 module.exports = events;
