4ec9100daf3d02d90561a9ee163e78cd3a295739
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-09 09:51:01 -0700

    Adjust time

--- schedules/helpdesk.js
+++ schedules/helpdesk.js
@@ -6,7 +6,7 @@ var date = require('../lib/date.js');
 var events = [];
 
 events.push({
-  when: date('W2D3', 08, 00),
+  when: date('W2D3', 09, 00),
   who: c.staff_helpdesk,
   message: '<!channel> Good Morning Helpdesk! Today the juniors embark on n-queens. Check out these notes to help you through this sprint: https://github.com/remotebeta-labs/docs.instruction/blob/master/sprint.n-queens.md'
 });
