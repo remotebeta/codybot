c93e916a4f4b1f1a4aeb6daf7d554a989d007e76
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-16 10:10:52 -0600

    Add Reboot Command

---
+++ commands/reboot.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var exec = require('child_process').exec;
+
+module.exports = reboot;
+
+function reboot(slack, args, message) {
+  var channel = slack.getChannelGroupOrDMByID(message.channel);
+  channel.send('codybot is going down and hopefully coming back up.');
+  exec('pm2 restart codybot', function (err, stdout, stderr) {
+    console.error(err);
+    console.error(stderr);
+    console.log(stdout);
+  });
+  // console.log(message._client.channels.C0LFUN4F2.name);
+}
