3ddb6e158241d1a1b4e86a358b75133659ee6a2b
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-17 14:41:28 -0600

    Switch juniors to seniors for w9d4

--- schedules/W9D4.js
+++ schedules/W9D4.js
@@ -7,13 +7,13 @@ var events = [];
 
 events.push({
   when: date('W9D4', 13, 40),
-  who: c.juniors,
+  who: c.seniors,
   message: '<!channel> 5 Minutes until Thesis Presentations!'
 });
 
 events.push({
   when: date('W9D4', 13, 44),
-  who: c.juniors,
+  who: c.seniors,
   message: '<!channel> 1 Minute until Thesis Presentations!'
 });
 
