de2b4d110dc8c976b097ac0d3b167a9ef242fc94
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-17 12:11:51 -0600

    W3D4 & W9D4

---
+++ schedules/W3D4.js
@@ -0,0 +1,26 @@
+// 'use strict';
+
+var c = require('../lib/channels.js');
+var date = require('../lib/date.js');
+
+var events = [];
+
+events.push({
+  when: date('W3D4', 15, 00),
+  who: c.juniors,
+  message: '<!channel> Townhall in 15 minutes. Post your questions here: https://townhall.makerpass.com/'
+});
+
+events.push({
+  when: date('W3D4', 15, 10),
+  who: c.juniors,
+  message: '<!channel> 5 Minutes until TownHall. https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W3D4', 15, 14),
+  who: c.juniors,
+  message: '<!channel> 1 Minute until TownHall. https://zoom.us/j/685772​608'
+});
+
+module.exports = events;
