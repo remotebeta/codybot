157d6ef8feeef4ba4ef0bc3b2116c81e8cd0ee1e
Cody B. Daig <cody.daig@hackreactor.com>
2016-03-28 09:27:54 -0700

    W5D1

---
+++ schedules/W5D1.js
@@ -0,0 +1,56 @@
+// 'use strict';
+
+var c = require('../lib/channels.js');
+var date = require('../lib/date.js');
+
+var events = [];
+
+events.push({
+  when: date('W5D1', 09, 30),
+  who: c.juniors,
+  message: '<!channel> Half way through the Self-Assessment!'
+});
+
+events.push({
+  when: date('W5D1', 09, 50),
+  who: c.juniors,
+  message: '<!channel> 10 minutes remaining. Start wrapping up loose ends.'
+});
+
+events.push({
+  when: date('W5D1', 09, 55),
+  who: c.juniors,
+  message: '<!channel> 5 minutes remaining. Get your PR prepped and submitted.'
+});
+
+events.push({
+  when: date('W5D1', 10, 00),
+  who: c.juniors,
+  message: '<!channel> Self Assessment is complete! Verbalization practice starting in 5 minutes! https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W5D1', 10, 04),
+  who: c.juniors,
+  message: '<!channel> Verbalization practice starting in 1 minute! https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W5D1', 14, 00),
+  who: c.juniors,
+  message: '<!channel> Townhall in 15 minutes. Post your questions here: https://townhall.makerpass.com/'
+});
+
+events.push({
+  when: date('W5D1', 14, 10),
+  who: c.juniors,
+  message: '<!channel> 5 Minutes until TownHall. https://zoom.us/j/685772​608'
+});
+
+events.push({
+  when: date('W5D1', 14, 14),
+  who: c.juniors,
+  message: '<!channel> 1 Minute until TownHall. https://zoom.us/j/685772​608'
+});
+
+module.exports = events;
