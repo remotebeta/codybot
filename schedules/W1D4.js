e67f4cd1c9c1f5484286021fa9897b1fb1cdcbc2
Cody B. Daig <cody.daig@hackreactor.com>
2016-04-21 08:49:10 -0700

    W1D4

--- schedules/W1D4.js
+++ schedules/W1D4.js
@@ -8,7 +8,7 @@ var events = [];
 events.push({
   when: date('W1D4', 08, 45),
   who: c.juniors,
-  message: '<!channel> Townhall in 15 minutes. Post your questions here: https://townhall.makerpass.com/'
+  message: '<!channel> Townhall after your morning lecture. Post your questions here: https://townhall.makerpass.com/'
 });
 
 events.push({
@@ -18,31 +18,31 @@ events.push({
 });
 
 events.push({
-  when: new Date(2016, 02, 03, 12, 14, 0),
+  when: date('W1D4', 12, 14),
   who: c.juniors,
   message: '<!channel> 1 minute until Kitchen Time.'
 });
 
 events.push({
-  when: new Date(2016, 02, 03, 14, 15, 0),
+  when: date('W1D4', 14, 15),
   who: c.juniors,
   message: '<!channel> Townhall in 15 minutes. Post your questions here: https://townhall.makerpass.com/'
 });
 
 events.push({
-  when: new Date(2016, 02, 03, 14, 25, 0),
+  when: date('W1D4', 14, 25),
   who: c.juniors,
   message: '<!channel> 5 Minutes until TownHall. https://zoom.us/j/685772​608'
 });
 
 events.push({
-  when: new Date(2016, 02, 03, 14, 29, 0),
+  when: date('W1D4', 14, 29),
   who: c.juniors,
   message: '<!channel> 1 Minute until TownHall. https://zoom.us/j/685772​608'
 });
 
 events.push({
-  when: new Date(2016, 02, 03, 18, 30, 0),
+  when: date('W1D4', 18, 30),
   who: c.juniors,
   message: '<!channel> Dinner is over. Video Lecture Time.'
 });
