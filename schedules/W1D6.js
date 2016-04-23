7abeeb08a39583a59a9dd52e9d3bbea8a70200f1
Cody B. Daig <cody.daig@hackreactor.com>
2016-04-23 14:59:43 -0700

    W1D6

--- schedules/W1D6.js
+++ schedules/W1D6.js
@@ -30,33 +30,33 @@ events.push({
 });
 
 events.push({
-  when: date('W1D6', 15, 30),
+  when: date('W1D6', 15, 00),
   who: c.juniors,
   message: '<!channel> Townhall in 15 minutes. Post your questions here: https://townhall.makerpass.com/'
 });
 
 events.push({
-  when: date('W1D6', 15, 40),
+  when: date('W1D6', 15, 10),
   who: c.juniors,
   message: '<!channel> 5 Minutes until TownHall. https://zoom.us/j/685772​608'
 });
 
 events.push({
-  when: date('W1D6', 15, 44),
+  when: date('W1D6', 15, 14),
   who: c.juniors,
   message: '<!channel> 1 Minute until TownHall. https://zoom.us/j/685772​608'
 });
 
-events.push({
-  when: date('W1D6', 16, 40),
-  who: c.juniors,
-  message: '<!channel> 5 Minutes until the Live Lecture on jQuery and Open Source Contributions. https://zoom.us/j/685772​608'
-});
-
-events.push({
-  when: date('W1D6', 16, 44),
-  who: c.juniors,
-  message: '<!channel> 1 Minute until the Live Lecture on jQuery and Open Source Contributions. https://zoom.us/j/685772​608'
-});
+// events.push({
+//   when: date('W1D6', 16, 40),
+//   who: c.juniors,
+//   message: '<!channel> 5 Minutes until the Live Lecture on jQuery and Open Source Contributions. https://zoom.us/j/685772​608'
+// });
+//
+// events.push({
+//   when: date('W1D6', 16, 44),
+//   who: c.juniors,
+//   message: '<!channel> 1 Minute until the Live Lecture on jQuery and Open Source Contributions. https://zoom.us/j/685772​608'
+// });
 
 module.exports = events;
