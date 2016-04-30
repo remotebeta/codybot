6b1f80ddcdcd29f5b541b049d4fead428412af8c
Thr1ve <fict1on.buhler@gmail.com>
2016-04-30 19:17:50 -0400

    update help command

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,6 @@ module.exports = function help(controller) {
 Usage: \`@codybot: <command>\`
 Commands:
 moo - Moos the current channel 5 times. Limit one use per channel per 30 min.
-reject <companyName> - Records that you were rejected from that company.
 caps <@user> - Repeats that user's last message in this channel, in ALL CAPS
 help - Prints this prompt`
     );
