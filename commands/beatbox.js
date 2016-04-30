1d2fc2940d24c43c2be6b7bb2f1a36c7bdebeeff
Thr1ve <fict1on.buhler@gmail.com>
2016-04-30 18:23:11 -0400

    update commands

--- commands/beatbox.js
+++ commands/beatbox.js
@@ -2,7 +2,6 @@
 
 module.exports = function beatbox(controller) {
   controller.hears('beatbox', ['direct_mention'], function(bot, message) {
-    console.log(message);
     bot.reply(message, 'Boots and Cats and Boots and Cats and Boots and Cats and Boots and Cats and Cats and Boots and Cats and Boots and Cats and Boots and Cats and Boots.');
   });
 }
---
+++ commands/buzz.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mooTracker = {};
+
+module.exports = function buzz(controller) {
+  controller.hears('moo', ['direct_mention'], function(bot, message) {
+    var okayToSend = true;
+    var lastVisit = mooTracker[message.channel];
+    if (lastVisit) {
+      var thirtyMinutes = 30 * 60000; // 60000 being the number of milliseconds in a minute
+      var now = new Date();
+      var thirtyMinutesAgo = new Date(now - thirtyMinutes);
+      if (lastVisit > thirtyMinutesAgo) {
+        okayToSend = false;
+      }
+    }
+
+    if(okayToSend) {
+      mooTracker[message.channel] = new Date();
+      for(var i=0; i < 5; i++) {
+        setTimeout(function () {
+          channel.send('<!channel> bzzzzzz!!!!!');
+        }, i * 2000);
+      }
+    }
+  });
+}
