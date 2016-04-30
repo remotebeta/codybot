8a26e0def5abaff267829d2dd379ebe2b87011fc
Thr1ve <fict1on.buhler@gmail.com>
2016-04-30 19:40:55 -0400

    woops

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ Usage: \`@codybot: <command>\`
 Commands:
 moo - Moo the current channel 5 times. Limit one use per channel per 30 min.
 buzz - Buzz the current channel 5 times. Limit one use per chanel per 30 min.
-morning - Say goodmorning!
+morning - Say good morning!
 compose - Print out a poem
 beatbox - Make Codybot drop a sick beat
 help - Print this prompt`
