4da851a9f7b407687a56b30d914cf17af6d8ac07
Thr1ve <fict1on.buhler@gmail.com>
2016-04-30 18:53:22 -0400

    update reboot with botkit

--- commands/reboot.js
+++ commands/reboot.js
@@ -2,15 +2,14 @@
 
 var exec = require('child_process').exec;
 
-module.exports = function() {};
-
-// function reboot(slack, args, message) {
-//   var channel = slack.getChannelGroupOrDMByID(message.channel);
-//   channel.send('codybot is going down and hopefully coming back up.');
-//   exec('pm2 restart codybot', function (err, stdout, stderr) {
-//     console.error(err);
-//     console.error(stderr);
-//     console.log(stdout);
-//   });
-//   // console.log(message._client.channels.C0LFUN4F2.name);
-// }
+module.exports = function reboot(controller) {
+  controller.hears('reboot', ['direct_mention'], function(bot, message) {
+    bot.reply(message, 'codybot is going down and hopefully coming back up.');
+    exec('pm2 restart codybot', function (err, stdout, stderr) {
+      console.error(err);
+      console.error(stderr);
+      console.log(stdout);
+    });
+    // console.log(message._client.channels.C0LFUN4F2.name);
+  })
+}
