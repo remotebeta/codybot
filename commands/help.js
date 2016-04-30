3e13a3cb103a6ec016eb3ffc9991a2777a87b870
Gordon Buhler <Thr1ve@users.noreply.github.com>
2016-04-30 19:42:25 -0400

    Clean up help command and add botkit link to readme (#14)
    * update help command
    
    * update help command
    
    * add link to botkit to readme
    
    * woops

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,12 @@ module.exports = function help(controller) {
 `I'm codybot!
 Usage: \`@codybot: <command>\`
 Commands:
-moo - Moos the current channel 5 times. Limit one use per channel per 30 min.
-reject <companyName> - Records that you were rejected from that company.
-caps <@user> - Repeats that user's last message in this channel, in ALL CAPS
-help - Prints this prompt`
+moo - Moo the current channel 5 times. Limit one use per channel per 30 min.
+buzz - Buzz the current channel 5 times. Limit one use per chanel per 30 min.
+morning - Say good morning!
+compose - Print out a poem
+beatbox - Make Codybot drop a sick beat
+help - Print this prompt`
     );
   });
 }
