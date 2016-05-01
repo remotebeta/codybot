45e6d8f30e08af30a85c25f24c91ae2be6835c1f
Kevin Chen <kevnchn@gmail.com>
2016-04-30 22:54:55 -0400

    Add a 'simon says' command (#17)
    * Add a 'simon says' command
    
    * Fix styling and naming

---
+++ commands/simonSays.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var prefix = [ "Simon says type the word... ", "Type the word... " ];
+var commands = [ "YES", "NO", "GO", "STOP" ];
+
+// Returns a random element from an array
+var randElem = function(array) {
+  var index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
+
+module.exports = function simonSays(controller) {
+  controller.hears('simonsays', ['direct_mention'], function(bot, message) {
+
+    var simonAsk = function(response, convo) {
+      var simonSays = [randElem(prefix), randElem(commands)];
+      convo.ask(simonSays, function(response, convo) {
+        if (simonSays[0] && response.text === simonSays[1]) {
+          convo.say('Good job!');
+          simonAsk(response, convo);
+        } else {
+          convo.say('Game Over!');
+        }
+        convo.next();
+      });
+    }
+
+    bot.startConversation(message, simonAsk);
+  });
+}
+
