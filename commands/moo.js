641067e4e9019a993c975c6ba78b3a01a4527529
Cody B. Daig <cody.daig@hackreactor.com>
2016-05-14 16:37:30 -0700

    SA

--- commands/moo.js
+++ commands/moo.js
@@ -4,6 +4,17 @@ var mooTracker = {};
 
 module.exports = function moo(controller) {
   controller.hears('moo', ['direct_mention'], function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'cow2',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
     var okayToSend = true;
     var thirtyMinutes = 30 * 60000; // 60000 being the number of milliseconds in a minute
     var now = new Date();
