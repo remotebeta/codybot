4d0fe6f6942bbcfea694c1909e7662dd017d0af7
Cody B. Daig <cody.daig@hackreactor.com>
2016-05-14 16:35:18 -0700

    SA

--- commands/morning.js
+++ commands/morning.js
@@ -2,6 +2,15 @@
 
 module.exports = function morning(controller) {
   controller.hears('morning', ['direct_mention'], function(bot, message) {
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'sun_with_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
     bot.reply(message, 'Goooooooooood Morning!!!!!!!!!!');
   });
 }
