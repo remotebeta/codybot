7948f9ee2ca6aa9716fd2786c74699876e39ac0f
Cody B. Daig <cody.daig@hackreactor.com>
2016-06-11 09:12:55 -0700

    Modify reboot command to also reboot scheduler

--- staffCommands/reboot.js
+++ staffCommands/reboot.js
@@ -5,11 +5,19 @@ var exec = require('child_process').exec;
 module.exports = function reboot(controller) {
   controller.hears('reboot', ['direct_mention'], function(bot, message) {
     bot.reply(message, 'codybot is going down and hopefully coming back up.');
-    exec('pm2 restart codybot', function (err, stdout, stderr) {
+    exec('pm2 restart codybotScheduler', function (err, stdout, stderr) {
       console.error(err);
       console.error(stderr);
       console.log(stdout);
+
+      exec('pm2 restart codybot', function (err, stdout, stderr) {
+        console.error(err);
+        console.error(stderr);
+        console.log(stdout);
+      });
+
     });
+
     // console.log(message._client.channels.C0LFUN4F2.name);
   })
 }
