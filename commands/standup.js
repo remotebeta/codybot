672a17e0bb3ab9705dc58a9b2d6c530e8791d898
Cody B. Daig <cody.daig@hackreactor.com>
2016-06-23 08:30:44 -0700

    Standup and Register Feature

---
+++ commands/standup.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var exec = require('child_process').exec;
+
+var $$users = {};
+var standups;
+
+module.exports = function reboot(controller, db) {
+
+  standups = db.collection('standups');
+
+  controller.hears(['standup'], ['direct_message','mention','direct_mention', 'message_received'], function(bot, message) {
+    bot.api.users.info({user: message.user}, function (err, user) {
+      $$users[message.user] = user.user;
+      // console.log(user);
+      // convo.$user = user;
+      bot.reply(message, 'Hello ' + user.user.real_name + '!');
+      bot.startPrivateConversation(message, step1);
+    });
+  });
+};
+
+function step1(response, convo) {
+  convo.ask('What did you do yesterday?', function(response, convo) {
+    step2(response, convo);
+    convo.next();
+  });
+}
+
+function step2(response, convo) {
+  convo.ask('What will you do today?', function(response, convo) {
+    step3(response, convo);
+    convo.next();
+  });
+}
+
+function step3(response, convo) {
+  convo.ask('Do you have any pain points / road blocks?', function(response, convo) {
+    step4(response, convo);
+    convo.next();
+  });
+}
+
+function step4(response, convo) {
+  convo.ask('What is 1 win that you have experienced in the last 24 hours?', function(response, convo) {
+    step5(response, convo);
+    convo.next();
+  });
+}
+
+function step5(response, convo) {
+
+  var obj = {};
+  obj.userID = convo.responses['What did you do yesterday?'].user;
+  obj.userName = $$users[obj.userID].real_name;
+  obj.timestamp = Date.now();
+  obj.responses = convo.responses;
+  console.log(obj);
+
+  standups.insertMany([obj], function(err, result) {
+    if(err) {
+      convo.say('Whoops. Something happened and your standup was unable to be saved to the database. Please try again. If the error persists please contact remote.beta.communication@')
+    } else {
+      convo.say('Thank you!');
+    }
+    convo.next();
+  });
+}
--- index.js
+++ index.js
@@ -4,22 +4,49 @@ var staffCommands = require('./staffCommands');
 var controller = slack.controller;
 var staffController = slack.staffController;
 
-// Commands will be an object that looks something like this:
-// {
-//   beatbox: [Function: beatbox],
-//   caps: [Function: caps],
-//   compose: [Function: compose],
-//   help: [Function: help],
-//   moo: [Function: moo],
-//   morning: [Function: morning],
-//   test: [Function: test],
-//   yo: [Function: yo]
-// }
-for (var command in commands) {
-  commands[command](controller);
-  commands[command](staffController);
-}
+var MongoClient = require('mongodb').MongoClient;
 
-for (var command in staffCommands) {
-  staffCommands[command](staffController);
-}
+var url = 'mongodb://localhost:27017/codybot';
+MongoClient.connect(url, function(err, db) {
+  // Commands will be an object that looks something like this:
+  // {
+  //   beatbox: [Function: beatbox],
+  //   caps: [Function: caps],
+  //   compose: [Function: compose],
+  //   help: [Function: help],
+  //   moo: [Function: moo],
+  //   morning: [Function: morning],
+  //   test: [Function: test],
+  //   yo: [Function: yo]
+  // }
+  for (var command in commands) {
+    commands[command](controller, db);
+    commands[command](staffController, db);
+  }
+
+  for (var command in staffCommands) {
+    staffCommands[command](staffController, db);
+  }
+});
+
+// staffController.on('message_received', function(bot, message) {
+//   if(message.type === 'hello') {
+//     startTest(bot);
+//   }
+// });
+//
+// var users = ['U0J6WJYE5', 'U0YTST37B', 'U1CBC3090']; // cody, cara, bianca
+// users = ['U0J6WJYE5'];
+//
+// function startTest(bot){
+//   users.forEach(function (userID){
+//     bot.startPrivateConversation({user: userID}, function(response, convo){
+//       convo.ask('Testing the stand-up feature. So...... What did you do today?', function(response, convo){
+//         convo.say('Sweet! Thanks for helping with the test.');
+//         console.log(convo.responses);
+//         convo.next();
+//       });
+//     });
+//   });
+//
+// }
