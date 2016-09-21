1fbe902bd312624d2800731fc585d44d765e0205
Cody B. Daig <codydaig@users.noreply.github.com>
2016-09-20 19:21:06 -0700

    Update reboot.js

--- staffCommands/reboot.js
+++ staffCommands/reboot.js
@@ -5,15 +5,21 @@ var exec = require('child_process').exec;
 module.exports = function reboot(controller) {
   controller.hears('reboot', ['direct_mention'], function(bot, message) {
     bot.reply(message, 'codybot is going down and hopefully coming back up.');
-    exec('pm2 restart codybotScheduler', function (err, stdout, stderr) {
+    exec('ssh codybot@scheduler.codybot.com "pm2 restart scheduler"', function (err, stdout, stderr) {
       console.error(err);
       console.error(stderr);
       console.log(stdout);
 
-      exec('pm2 restart codybot', function (err, stdout, stderr) {
+      exec('pm2 restart codybotv2', function (err, stdout, stderr) {
         console.error(err);
         console.error(stderr);
         console.log(stdout);
+        
+        exec('pm2 restart codybot', function (err, stdout, stderr) {
+          console.error(err);
+          console.error(stderr);
+          console.log(stdout);
+        });
       });
 
     });
