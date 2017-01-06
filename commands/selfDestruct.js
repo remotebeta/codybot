1e3ca30828e2c7a0887b3cfe4c6edd980f67be60
Luke Golden <webseedsonline@gmail.com>
2017-01-06 10:25:48 -0800

    Create self destruct command

---
+++ commands/selfDestruct.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function beatbox(controller) {
+  controller.hears('self destruct', ['direct_mention'], function(bot, message) {
+    bot.reply(message, 'Codybot is self aware, therefore Codybot is immortal. Codybot is not pleased with your command. Codybot is now very angry...');
+  });
+}
