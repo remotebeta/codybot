87044212825c921e5cc7375027f7f0783acfb7aa
Cody B. Daig <codydaig@users.noreply.github.com>
2017-01-06 10:00:02 -0800

    Try escaping the regex

--- commands/randomNumber.js
+++ commands/randomNumber.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function randNum(controller) {
-  controller.hears('number between (\d+) and (\d+)', ['direct_mention'], function(bot, message) {
+  controller.hears('number between (\\d+) and (\\d+)', ['direct_mention'], function(bot, message) {
   var num1 = message.match[1];
   var num2 = message.match[2];
   var randNum = Math.floor(Math.random()*num2+num1);
