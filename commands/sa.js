bf7e23827e2aa912b6431e906763ceaa871f7640
Neil Murphy <nmurphy@gopro.com>
2017-01-06 09:58:44 -0800

    Revert "Fixed the response to any mention of the technical (summary) assessment."
    This reverts commit 36ab954db67f20fbb0612d10d2017bb4df893878.

--- commands/sa.js
+++ commands/sa.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 
 module.exports = function reboot(controller) {
   controller.hears('technical assessment', ['direct_message','mention','direct_mention', 'message_received'], function(bot, message) {
-    bot.reply(message, 'May god have mercy on your souls...');
+    bot.reply(message, 'no comment');
     // console.log(message._client.channels.C0LFUN4F2.name);
   });
 }
