08a0d0d747817c7fcb99702edc0d5ace3bf987ea
d-burke <omnidan@yahoo.com>
2017-01-05 21:32:35 -0800

    Codybot can give random number between range

--- commands/coinFlip.js
+++ commands/coinFlip.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function yo(controller) {
+module.exports = function coinFlip(controller) {
   controller.hears('flip a coin', ['direct_mention'], function(bot, message) {
     var num = Math.floor(Math.random()*2+1);
     num === 1 ? bot.reply(message, 'Coin flip result: Heads!') : bot.reply(message, 'Coin flip result: Tails!');
