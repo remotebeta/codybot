05fe9ce5fb9f967acfbc41dc7f5b7431fcd2a148
Cody B. Daig <cody@daig.me>
2017-02-16 20:55:49 -0700

    Remove Answers from Question

--- commands/joke.js
+++ commands/joke.js
@@ -3,14 +3,14 @@
 module.exports = function joke(controller) {
   controller.hears('joke', ['direct_mention'], function(bot, message) {
     var tellQ = [
-      'Q: How do you comfort a JavaScript bug? A: You console it',
-      'Q: Why was the JavaScript developer sad? A: Because he didn’t Node how to Express himself',
-      'Q: Why did the developer go broke? A: Because he used up all his cache',
-      'Q: What\'s the object-oriented way to become wealthy? A: Inheritance',
-      'Q: What do you call a programmer from Finland? A: Nerdic',
-      'Q: What\'s a programmer\'s favorite place to hang out? A: Foo Bar',
-      'Q: Why did the programmer quit his job? A: He didn\'t get arrays.',
-      'Q: How many Computer Scientist students does it take to change a lightbulb? A: None, that\'s a hardware problem',
+      'Q: How do you comfort a JavaScript bug?',
+      'Q: Why was the JavaScript developer sad?',
+      'Q: Why did the developer go broke?',
+      'Q: What\'s the object-oriented way to become wealthy?',
+      'Q: What do you call a programmer from Finland?',
+      'Q: What\'s a programmer\'s favorite place to hang out?',
+      'Q: Why did the programmer quit his job?',
+      'Q: How many Computer Scientist students does it take to change a lightbulb?',
       'A SQL query goes into a bar, walks up to two tables and asks, "Can I join you?'
     ];
 
